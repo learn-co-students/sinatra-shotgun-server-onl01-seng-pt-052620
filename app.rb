a1eb117f08bf56cd3dc6b6c89423d781ac7dac19
rumikoacopa <rumikorios@gmail.com>
2020-08-18 21:32:11 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,9 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
-  end
-
+    "Started my server using Shotgun!"
+    end
+  #Once you make a change, shut server down(server is what controls the info to get to the 
+  #the website which is localhost:9292) local host by hitting ctrl c then 
+  #restart by running rackup app.rb
 end
